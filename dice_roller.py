cb07252d8ca551bbb2810df153883f894ffd62da
lipe <lipe_rodrigo@outlook.com>
2020-05-11 15:01:25 -0300

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,12 @@
+import random
+dice_rolls = 2
 def main():
-  print('You rolled a die')
+  dice_sum  =  0
+  for i in range (0, dice_rolls):
+    roll = random.randint(1, 6)
+    dice_sum += roll
+    print (' Você rolou um {} '.format(roll) )
+    print('Você rolou um total de ', dice_sum)
 
 if __name__== "__main__":
   main()
